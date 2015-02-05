a22fcdb36d91132640cc435b9603eed83475c951
sethwulbecker <seth.wulbecker@my.wheaton.edu>
2015-02-05 16:59:13 -0600

    Did some stuff

--- TestFunction.py
+++ TestFunction.py
@@ -19,19 +19,31 @@
   
   f = Function.Function
   
-  def testFunction_dx_(self):
+  def test_x_(self):
+   x2 = f.xn(2)
+   y4 = f.yn(4)
+   v = f.vectorize(x2, y4)
+   self.assertEqual(16.0, f.evaluate(v.x(), 4, 1))
+   
+  def test_y_(self):
+   x2 = f.xn(2)
+   y4 = f.yn(4)
+   v = f.vectorize(x2, y4)
+   self.assertEqual(81.0, f.evaluate(v.x(), 1, 3))
+  
+  def test_dx_(self):
    x3 = f.xn(3)
    self.assertEqual(12.0, f.evaluate(x3.dx(), 2, 1))
    
-  def testFunction_dy_(self):
+  def test_dy_(self):
    y3 = f.yn(3)
    self.assertEqual(48.0, f.evaluate(y3.dy(), 1, 4))
   
-  def testFunction_xn_(self):
+  def test_xn_(self):
    x2 = f.xn(2)
    self.assertEqual(9.0, f.evaluate(x2, 3, 1))
   
-  def testFunction_yn_(self):
+  def test_yn_(self):
    y2 = f.yn(2)
    self.assertEqual(25.0, f.evaluate(y2, 1, 5))
    
