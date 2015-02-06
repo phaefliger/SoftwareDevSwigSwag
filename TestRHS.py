7d2c0f6a24ab865fbd9a795da05158e97216941b
Connor de la Cruz <connor.delacruz@my.wheaton.edu>
2015-02-05 18:18:33 -0600

    added tests for RHS

--- TestRHS.py
+++ TestRHS.py
@@ -2,3 +2,10 @@ import RHS
 import unittest
 
 class TestRHS(unittest.TestCase):
+    
+    rhs = RHS.RHS
+
+    # constructors
+    def test_RHS_(self):
+        self.assertEqual(rhs.RHS(), rhs.RHS(false))
+        
