642f1a5042c3692e44593f232da4da106434827e
Connor de la Cruz <connor.delacruz@my.wheaton.edu>
2015-02-05 18:16:27 -0600

    cleaned things up a bit

--- TestSpatialFilter.py
+++ TestSpatialFilter.py
@@ -6,67 +6,67 @@ class TestSpatialFilter(unittest.TestCase):
     sf = SpatialFilter.SpatialFilter
 
     # MatchesPoint Test
-    def testFunc_matchesPoint_(self):
+    def test_matchesPoint_(self):
         x = sf.matchingX(2.0)
         y = sf.matchingY(4.0)
         xy = x | y
         self.assertEqual(True, xy.matchesPoint(2.0, 4.0))
 
     # allSpace test
-    def testFunc_allSpace(self):
+    def test_allSpace_(self):
         x = sf.matchingX(1.0)
         y = sf.negatedFilter(x)
         xy = x | y
         self.assertEqual(xy, sf.allSpace())
 
     # unionFilter
-    def testFunc_unionFilter(self):
+    def test_unionFilter_(self):
         x = sf.matchingX(36.0)
         y = sf.matchingY(64.0)
         xy = sf.unionFilter(x, y)
         self.assertEqual(True, xy.matchesPoint(36.0, 64.0)
         
     # intersectionFilter
-    def testFunc_intersectionFilter(self):
+    def test_intersectionFilter_(self):
         x = sf.greaterThanX(1.0)
         y = sf.allSpace()
         xy = sf.intersectionFilter(x, y)
         self.assertEqual(xy, x)
         
     # negatedFilter
-    def testFunc_negatedFilter(self):
+    def test_negatedFilter_(self):
         x = sf.matchingX(1.0)
         y = sf.negatedFilter(x)
         xy = x & y
         self.assertEqual(xy, sf.negatedFilter(sf.allSpace()))
 
     # matchingX
-    def testFunc_matchingX(self):
+    def test_matchingX_(self):
         x = sf.matchingX(8.0)
         self.assertEqual(True, x.matchesPoint(8.0, 24.0))
 
      # matchingY
-    def testFunc_matchingY(self):
+    def test_matchingY_(self):
         y = sf.matchingY(8.0)
         self.assertEqual(True, y.matchesPoint(24.0, 8.0))
 
     # lessThanX
-    def testFunc_lessThanX(self):
+    def test_lessThanX_(self):
         x = sf.lessThanX(8.0)
         self.assertEqual(False, x.matchesPoint(8.1, 24.0))
     
     # lessThanY
-    def testFunc_lessThanY(self):
+    def test_lessThanY_(self):
         x = sf.lessThanY(8.0)
         self.assertEqual(False, x.matchesPoint(24.0, 8.1))
     
     # greaterThanX
-    def testFunc_greaterThanX(self):
+    def test_greaterThanX_(self):
         x = sf.greaterThanX(8.0)
         self.assertEqual(True, x.matchesPoint(8.1, 24.0))
     
     # greaterThanY
-    def testFunc_greaterThanY(self):
+    def test_greaterThanY_(self):
         x = sf.greaterThanY(8.0)
         self.assertEqual(True, x.matchesPoint(24.0, 8.1))
     
