b5dd97e1b6f710d3bbcd219beadb24a6cfc565b7
Connor de la Cruz <connor.delacruz@my.wheaton.edu>
2015-02-05 18:27:17 -0600

    added more tests for RHS

--- TestRHS.py
+++ TestRHS.py
@@ -1,5 +1,6 @@
 import RHS
 import unittest
+import LinearTerm
 
 class TestRHS(unittest.TestCase):
     
@@ -9,3 +10,17 @@ class TestRHS(unittest.TestCase):
     def test_RHS_(self):
         self.assertEqual(rhs.RHS(), rhs.RHS(false))
         
+    # --- not sure how RHS works, need more info to write tests for nonZeroRHS and addTerm
+    
+    def test_linearTerm_(self):
+        r = rhs.RHS()
+        lt = r.linearTerm()
+        # --- need LinearTerm to be working so I know how to test it. will want to change the linearTerm and compare it to the RHS
+        self.assertEqual(r, lt)
+    
+    def test_linearTermCopy(self):
+        r = rhs.RHS()
+        lt = r.linearTermCopy()
+        # --- need LinearTerm to be working so I know how to test it. will want to change the linearTerm and compare it to the RHS
+        self.assertNotEqual(r, lt)
+    
