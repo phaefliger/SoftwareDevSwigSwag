25e236182f5f85b9985f5dbd8b99f05bb0b4b438
sethwulbecker <seth.wulbecker@my.wheaton.edu>
2015-02-11 21:25:32 -0600

    added function test

--- runTests.py
+++ runTests.py
@@ -1,9 +1,17 @@
-from TestAdder import * #this syntax imports whatever is defined in TestAdder into the current namespace
-from TestTimes import *
+#from TestAdder import * #this syntax imports whatever is defined in TestAdder into the current namespace
+#from TestTimes import *
+#import unittest
+
+#testSuite = unittest.makeSuite(TestAdder)
+#testSuite.addTest(unittest.makeSuite(TestTimes))
+
+#testRunner = unittest.TextTestRunner()
+#testRunner.run(testSuite)
+
+from TestFunction import *
 import unittest
 
-testSuite = unittest.makeSuite(TestAdder)
-testSuite.addTest(unittest.makeSuite(TestTimes))
+testSuite = unittest.makeSuite(TestFunction)
 
 testRunner = unittest.TextTestRunner()
 testRunner.run(testSuite)
