7179f6cdcbb91e607570173bc366cbaaceb3afb5
Seth P. Wulbecker <seth.wulbecker@cslab25.wheaton.edu>
2015-02-12 19:34:01 -0600

    finished function

---
+++ submission./Function.i
@@ -0,0 +1,111 @@
+%module Function
+%{
+#include "Function.h"
+%}
+
+%include "std_string.i"
+%include "std_vector.i"
+%include "Mesh.i"
+%include "Solution.i"
+
+namespace std {
+  %template(DoubleVector) vector<double>;
+  }
+
+%nodefaultctor Function;  // Disable the default constructor for class Function
+
+class Function {
+public:
+  std::string displayString();
+  double evaluate(double x);
+  double evaluate(double x, double y);
+  double evaluate(double x, double y, double z);
+  
+  virtual FunctionPtr x();
+  virtual FunctionPtr y();
+  virtual FunctionPtr div();
+  int rank();
+  double l2norm(MeshPtr mesh, int cubatureDegreeEnrichment = 0);
+  
+  
+  // member functions for taking derivatives:
+  FunctionPtr dx();
+  FunctionPtr dy();
+
+  FunctionPtr grad();
+
+  // static methods:
+  static double evaluate(FunctionPtr f, double x, double y); 
+  
+  static FunctionPtr xn(int n=1); // NOTE: important to have "FunctionPtr" here exactly as below; "Teuchos::RCP<Function>", though equivalent in C++, is not equivalent for SWIG
+  static FunctionPtr yn(int n=1);
+  
+  static FunctionPtr composedFunction(FunctionPtr f, FunctionPtr g);
+  static FunctionPtr constant(double value);
+  static FunctionPtr vectorize(FunctionPtr f1, FunctionPtr f2);
+  static FunctionPtr normal();
+  static FunctionPtr solution(VarPtr var, SolutionPtr soln);
+  
+
+  // SWIG/Python extensions:
+  %extend {
+    std::string __str__() {
+      return self->displayString();
+    }
+  }
+};
+
+//FunctionPtr operator*(double weight, FunctionPtr f);
+
+class FunctionPtr {
+public:
+  Function* operator->();
+
+  %extend {
+    FunctionPtr __add__(FunctionPtr f2) {
+      return *self + f2;
+    }
+    FunctionPtr __add__(double value) {
+      return *self + value;
+    }
+    FunctionPtr __radd__(double value) {
+      return *self + value;
+    }
+    FunctionPtr __mul__(double value) {
+      return *self * value;
+    }
+    FunctionPtr __rmul__(double value) {
+      return *self * value;
+    }
+    FunctionPtr __sub__(FunctionPtr f2) {
+      return *self - f2;
+    }
+    FunctionPtr __sub__(double value) {
+      return *self - value;
+    }
+    FunctionPtr __rsub__(double value) {
+      return value - *self;
+    }
+    FunctionPtr __mul__(FunctionPtr f2) {
+      return *self * f2;
+    }
+    FunctionPtr __div__(FunctionPtr scalarDivision) {
+      return *self / scalarDivision;
+    }
+    FunctionPtr __div__(double divisor) {
+      return *self / divisor;
+    }
+    FunctionPtr __rdiv__(double value) {
+      return value / *self;
+    }
+    FunctionPtr __mul__(vector<double> weight) {
+      return *self * weight;
+    }
+    FunctionPtr __rmul__(vector<double> weight) {
+      return weight * *self;
+    }
+    FunctionPtr __neg__() {
+      return -1 * *self;
+    }
+  }
+};
---
+++ submission./TestFunction.py
@@ -0,0 +1,200 @@
+#import Adder
+#import unittest
+
+#class TestAdder(unittest.TestCase):
+ # """Test Adder.py's addNumbers() method"""
+  #def testadd(self):
+   # self.assertAlmostEqual(8,Adder.addNumbers(5,3),delta=1e-12)
+    #self.assertEqual(8,Adder.addNumbers(5,3))
+  
+
+# Run the tests:
+#if (__name__ == '__main__'):
+ # unittest.main()
+ 
+ import Function
+ import VarFactory
+ import Var
+ mport BF
+ import MeshFactory
+ import unittest
+ 
+ class TestFunction(unittest.TestCase):
+  
+  f = Function.Function
+  
+  def test_x_(self):
+   x2 = f.xn(2)
+   y4 = f.yn(4)
+   v = f.vectorize(x2, y4)
+   self.assertAlmostEqual(16.0, f.evaluate(v.x(), 4, 1), delta=1e-12)
+   
+  def test_y_(self):
+   x2 = f.xn(2)
+   y4 = f.yn(4)
+   v = f.vectorize(x2, y4)
+   self.assertAlmostEqual(81.0, f.evaluate(v.x(), 1, 3), delta=1e-12)
+  
+  def test_dx_(self):
+   x3 = f.xn(3)
+   self.assertAlmostEqual(12.0, f.evaluate(x3.dx(), 2, 1), delta=1e-12)
+   
+  def test_dy_(self):
+   y3 = f.yn(3)
+   self.assertAlmostEqual(48.0, f.evaluate(y3.dy(), 1, 4), delta=1e-12)
+  
+  def test_div_(self):
+   x2 = f.xn(2)
+   y4 = f.yn(4)
+   v = f.vectorize(x2, y4)
+   self.assertAlmostEqual(40.0, f.evaluate(v.div(), 4, 2), delta=1e-12)
+   
+  def test_grad_(self):
+   x2 = f.xn(2)
+   y4 = f.yn(4)
+   v = f.vectorize(x2, y4)
+   self.assertEqual("(2 \\cdot x,0)", v.grad().x().displayString())
+   self.assertEqual("(0,4 \\cdot y^3)", v.grad().y().displayString())
+   
+  def test_rank_(self):
+   x2 = f.xn(2)
+   y4 = f.yn(4)
+   v = f.vectorize(x2, y4)
+   self.assertEqual(0, x2.rank())
+   self.assertEqual(1, v.rank())
+   
+  def test_l2norm_(self):
+   x2 = f.xn(2)
+   y4 = f.yn(4)
+   g = f.vectorize(x2, y4)
+   vf = VarFactory.VarFactory()
+   p = vf.fieldVar("p")
+   v = vf.testVar("v", Var.HGRAD)
+   b = BF.BF_bf(vf)
+   mp = MeshFactory.MeshFactory_rectilinearMesh(b, [1.0, 1.0], [1, 1], 2)
+   self.assertAlmostEqual(0.44721359549995804, x2.l2norm(mp, 0), delta=1e-12))
+   self.assertAlmostEqual(0.3293301281895316, y4.l2norm(mp, 0), delta=1e-12))
+   self.assertAlmostEqual(0.5553902531853916, g.l2norm(mp, 0), delta=1e-12))
+   
+  def test_displayString_(self):
+   x2 = f.xn(2)
+   y4 = f.yn(4)
+   v = f.vectorize(x2, y4)
+   self.assertEqual("x^2", x2.displayString())
+   self.assertEqual("y^4", y4.displayString())
+   self.assertEqual("(x^2,y^4)", v.displayString())
+   
+  def test_evaluate_(self):
+   x2 = f.xn(2)
+   y3 = f.yn(3)
+   self.assertAlmostEqual(31.0, f.evaluate(x2+y3, 2, 3), delta=1e-12)
+   
+  def test_composedFunction_(self):
+   h = 3*f.xn(2) + 5*f.yn(1)
+   g = f.vectorize(3*f.yn(1), 2*f.xn(1) + 3)
+   self.assertAlmostEqual(153.0, f.evaluate(f.composedFunction(h, g), 3, 2), delta=1e-12)
+   
+  def test_constant_(self):
+   self.assertEqual("7", f.constant(7).displayString())
+   
+  def test_vectorize_(self):
+   x2 = f.xn(2)
+   y4 = f.yn(4)
+   v = f.vectorize(x2, y4)
+   self.assertEqual("x^2", v.x().displayString())
+   self.assertEqual("y^4", v.y().displayString())
+   
+  #Told NOT to test!!
+  #def test_normal_(self):
+   
+  def test_solution(self):
+   vf = VarFactory.VarFactory()
+   p = vf.fieldVar("p")
+   v = vf.testVar("v", Var.HGRAD)
+   b = BF.BF_bf(vf)
+   mp = MeshFactory.MeshFactory_rectilinearMesh(b, [1.0, 1.0], [1, 1], 2)
+   soln = Solution.Solution_solution(mp)
+   soln.projectOntoMesh({p.ID() : f.xn(500)})
+   g = f.solution(p, soln)
+   self.assertAlmostEqual(0.0, f.xn(500).l2norm(mp, 0) - g.l2norm(mp, 0), delta=1e-12)
+   
+  
+  def test_xn_(self):
+   x2 = f.xn(2)
+   self.assertAlmostEqual(9.0, f.evaluate(x2, 3, 1), delta=1e-12)
+  
+  def test_yn_(self):
+   y2 = f.yn(2)
+   self.assertAlmostEqual(25.0, f.evaluate(y2, 1, 5), delta=1e-12)
+   
+  def test_multFF_(self):
+   x2 = f.xn(2)
+   y2 = f.yn(2)
+   self.assertAlmostEqual(36.0, f.evaluate(x2*y2, 2, 3), delta=1e-12)
+   
+  def test_divideFF_(self):
+   x2 = f.xn(2)
+   y2 = f.yn(2)
+   self.assertAlmostEqual(4.0, f.evaluate(x2/y2, 4, 2), delta=1e-12)
+   
+  def test_divideFD_(self):
+   x2 = f.xn(2)
+   self.assertAlmostEqual(8.0, f.evaluate(x2/2, 4, 1), delta=1e-12)
+   
+  def test_divideDF_(self):
+   x2 = f.xn(2)
+   self.assertAlmostEqual(2.0, f.evaluate(18/x2, 3, 1), delta=1e-12)
+   
+  def test_multDF_(self):
+   x2 = f.xn(2)
+   self.assertAlmostEqual(36.0, f.evaluate(4*x2, 3, 1), delta=1e-12)
+   
+  def test_multFD_(self):
+   x2 = f.xn(2)
+   self.assertAlmostEqual(50.0, f.evaluate(x2*2, 5, 1), delta=1e-12)
+   
+  def test_multVF_(self):
+   x2 = f.xn(2)
+   y4 = f.yn(4)
+   h = [3.0, 2.0]
+   g = f.vectorize(x2, y4)
+   self.assertAlmostEqual(59.0, f.evaluate(h * g, 3, 2), delta=1e-12)
+   
+  def test_multFV_(self):
+   x2 = f.xn(2)
+   y4 = f.yn(4)
+   h = [3.0, 2.0]
+   g = f.vectorize(x2, y4)
+   self.assertAlmostEqual(174.0, f.evaluate(g * h, 2, 3), delta=1e-12)
+   
+  def test_addFF_(self):
+   x2 = f.xn(2)
+   y4 = f.yn(4)
+   self.assertAlmostEqual(32.0, f.evaluate(x2+y4, 4, 2), delta=1e-12)
+   
+  def test_addFD_(self):
+   x2 = f.xn(2)
+   self.assertAlmostEqual(19.0, f.evaluate(x2+3, 4, 2), delta=1e-12)
+   
+  def test_addDF_(self):
+   x2 = f.xn(2)
+   self.assertAlmostEqual(27.0, f.evaluate(2+x2, 5, 2), delta=1e-12)
+   
+  def test_minFF_(self):
+   x2 = f.xn(2)
+   y4 = f.yn(4)
+   self.assertAlmostEqual(0.0, f.evaluate(x2-y4, 4, 2), delta=1e-12)
+   
+  def test_minFD_(self):
+   x2 = f.xn(2)
+   self.assertAlmostEqual(47.0, f.evaluate(x2-2, 7, 2), delta=1e-12)
+   
+  def test_minDF_(self):
+   x2 = f.xn(2)
+   self.assertAlmostEqual(25.0, f.evaluate(50-x2, 5, 2), delta=1e-12)
+   
+  def test_neg_(self):
+   x2 = f.xn(2)
+   self.assertAlmostEqual(-25.0, f.evaluate(-x2, 5, 2), delta=1e-12)
+  
+   
